78a3337c0b244c82ac70cfb3e2d98db73d099354
Tali <allysonnomuraga@gmail.com>
2024-01-16 00:09:56 -0500

    [MAYOR CHANGE] added cards

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,20 +15,23 @@ const config: Config = {
       switzer: ['Switzer', 'sans-serif']
     },
 
-    colors: {
-      primary: '#06C',
-
-      gray: {
-        100: '#363B42',
-        200: '#2E2E2F'
-      },
-
-      white: '#FFF',
-      black: '#060606',
-      transparent: 'transparent'
-    },
-
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#06C',
+  
+        gray: {
+          100: '#363B42',
+          200: '#2E2E2F'
+        },
+  
+        white: '#FFF',
+        black: '#060606',
+        transparent: 'transparent'
+      }
+  
+      
+      
+    }
   },
 
   plugins: []
