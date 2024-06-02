17ae14d47f3920957533690af685fd7563e0daa1
yigs <yigsvnsla@gmail.com>
2024-06-02 10:03:47 -0500

    initial commit Co-authored-by: Tali <TaliAly@users.noreply.github.com> Co-authored-by: Danilo Antonio M. <dantech99@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,17 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['es', 'en']
+  },
+  integrations: [
+    tailwind({
+      nesting: true,
+      applyBaseStyles: true
+    }),
+    icon()
+  ],
   build: {
     assets: 'assets'
   }
