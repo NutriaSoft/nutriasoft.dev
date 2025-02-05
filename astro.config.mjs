8e29b25d5058234e6a4618b0cfea285ad54c4e69
Erick Vargas <erianvc.dev@gmail.com>
2025-02-04 23:34:58 -0500

    refactor: update tailwindcss to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import icon from 'astro-icon'
 import vercel from '@astrojs/vercel'
-import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 import { typings } from './scripts/integrations'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), typings()],
+  integrations: [icon(), typings()],
   output: 'server',
   adapter: vercel(),
   build: {
     assets: 'assets'
+  },
+  vite: {
+    plugins: [tailwindcss()],
   }
 })
